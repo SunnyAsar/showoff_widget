98b64bd072722706a96e4f66fe9efe39e9d3d16d
Sunny Asar <ring2rings93@gmail.com>
2020-03-20 22:02:41 +0100

    added search form and methods to search for widget

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -5,12 +5,28 @@ class WidgetsController < ApplicationController
     @item = ENV['CLIENT_SECRET']
     @user = session[:user]
     @auth = session[:token]["access_token"]
-
-
-    response = Widget.get_widgets @auth
-     @widgets =  response['data']['widgets']
+     if params[:search]
+      find_widget
+     else
+      response = Widget.get_widgets @auth
+      @widgets = response['data']['widgets']
+     end
   end
 
   def show
+  end
+
+  private
+
+  def find_widget
+    term = search_params[:search_term]
+    if term
+      response = Widget.search(term, @auth)
+      @widgets = response['data']['widgets']
+    end
+  end
+
+  def search_params
+    params.require(:search).permit(:search_term)
   end
 end
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -2,16 +2,29 @@ class Widget < ApplicationRecord
   require 'rest-client'
   require 'json'
 
-  def self.get_widgets token
-    c_Id = ENV['CLIENT_ID']
-    c_Secret = ENV['CLIENT_SECRET']
-    url = "https://showoff-rails-react-production.herokuapp.com/api/v1/widgets/visible?client_id=#{c_Id}&client_secret=#{c_Secret}"
-    
+  def self.get_widgets(token)
+    c_id = ENV['CLIENT_ID']
+    c_secret = ENV['CLIENT_SECRET']
+    url = "https://showoff-rails-react-production.herokuapp.com/api/v1/widgets/visible?client_id=#{c_id}&client_secret=#{c_secret}"
+
     response = RestClient::Request.execute(method: :get,
                             url: url,
-                            headers: {"Content-Type" => "application/json", :Authorization => "Bearer #{token}" }
-                          )
-                  return JSON.parse response
-    end
+                            headers: {"Content-Type" => "application/json",
+                             :Authorization => "Bearer #{token}" }
+                                          )
+     JSON.parse response
+  end
 
+  def self.search(term, token)
+    c_id = ENV['CLIENT_ID']
+    c_secret = ENV['CLIENT_SECRET']
+    url = "https://showoff-rails-react-production.herokuapp.com/api/v1/widgets/visible?client_id=#{c_id}&client_secret=#{c_secret}&term=#{term}"
+
+    response = RestClient::Request.execute(method: :get,
+                                url: url,
+                                headers: {"Content-Type" => "application/json",
+                                :Authorization => "Bearer #{token}" }
+                                 )
+                                JSON.parse response
+  end
 end
